c45f892b780cd7d913a270bfc4d5c76afb47e8e1
Chemar Wright <chemarrian@gmail.com>
2020-09-16 19:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+funtion (happyHolidays) { return ("Happy Holidays")!}
+var Name = "Chemar"
+funtion sayHappyHolidaysTo ("Name") { return `Happy holidays, ${name}!`
+
+var Holiday = "Christmas"
+Var Days = "7"
+funtion holidaycountdown("Christmas, 7 ")
+console.log (`Its ${Days} until ${Holiday}!`)
