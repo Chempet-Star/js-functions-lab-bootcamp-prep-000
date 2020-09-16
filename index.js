e0e974beaa2b57f32e0f7f107432a16e9cd1818c
Chemar Wright <chemarrian@gmail.com>
2020-09-16 19:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ funtion sayHappyHolidaysTo ("Name") { return `Happy holidays, ${name}!`
 var Holiday = "Christmas"
 Var Days = "7"
 funtion holidaycountdown("Christmas, 7 ")
-console.log (`Its ${Days} until ${Holiday}!`)
+console.log (`Its ${3+4} until ${Holiday}!`)
